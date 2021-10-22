f4526e1f393bf131c9af50e277ce9207bb25b606
VBel3235 <Vlad95.0753@gmail.com>
2021-10-22 20:24:56 +1000

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Владислав Белов on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
